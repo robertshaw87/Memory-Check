0888bef8813acbe8ea82193c1eb17dd79a65ef92
Robert Shaw <robertshaw87@gmail.com>
2018-06-25 23:48:33 -0700

    finish navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,29 +3,23 @@ import "./Navbar.css";
 
 const Navbar = props => (
   <nav className="navbar navbar-light mb-3">
-    <span className="navbar-brand mb-0">
-      <span><img src="/images/brain.png" alt="Brain" className="animated bounce"/></span>
-      Memory Game
+    <span className="navbar-brand py-0 mb-0">
+      <span><img src="/images/brain.png" alt="Brain" className="animated bounce mr-2 d-none d-md-inline-block"/></span>
+      <span className="brand-text">Memory Game</span>
     </span>
-    <div className="card p-2">
-      <div className="card-body p-0">
-        <div className="card-text p-0 container-fluid">
-          <div className="row m-0 bg-primary">
-            <div className="col-10 p-0">
-              Current Score:&nbsp;
-          </div>
-          <div className="col-2 text-right p-0">{props.currScore}</div>
-          </div>
-          <div className="row m-0 bg-primary">
-            <div className="col-10 p-0">
-              High Score:&nbsp;
-          </div>
-          <div className="col-2 text-right p-0">{props.highScore}</div>
-          </div>
-        </div>
+    <span className="score-display mb-2 m-0 mx-sm-5">
+    <div className="container">
+      <div className="row m-0">
+        <div className="col-11 p-0 score-text">Current Score:&nbsp;</div>
+        <div className="col-1 text-right p-0 score-text">{props.currScore}</div>
+      </div>
+      <div className="row m-0">
+        <div className="col-11 p-0 score-text">High Score:&nbsp;</div>
+        <div className="col-1 text-right p-0 score-text">{props.highScore}</div>
       </div>
     </div>
-    <button type="button" className="btn btn-dark py-2 px-4" onClick={props.resetGame}>Reset Game</button>
+    </span>
+    <button type="button" className="btn btn-dark py-2 px-4 ml-lg-5" onClick={props.resetGame}>Reset Game</button>
   </nav>
 );
 
