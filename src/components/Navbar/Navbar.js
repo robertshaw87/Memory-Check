99dd3f9e99cda868c8878b67d69c3c01faa01631
Robert Shaw <robertshaw87@gmail.com>
2018-06-25 20:28:18 -0700

    add background styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props => (
-  <nav className="navbar navbar-light bg-light">
+  <nav className="navbar navbar-light bg-light mb-3">
     <span className="navbar-brand mb-0 h1">Memory Game</span>
     <p className="mt-3">Current Score: {props.currScore} ||  High Score: {props.highScore}</p>
     <button type="button" className="btn btn-primary" onClick={props.resetGame}>Reset Game</button>
